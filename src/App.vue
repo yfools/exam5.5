51dbd33833e6deb9d15f6975925b0d93878eff2c
yfools <1359318059@qq.com>
2022-05-12 22:34:55 +0800

    wip: change

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,39 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
 
+  mounted() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://www.baidu.com");
+    xhr.send();
+    this.screenChange();
+  },
+
+  methods: {
+    screenChange() {
+      var mql = window.matchMedia("(orientation: portrait)");
+      function onMatchMeidaChange(mql) {
+        if (mql.matches) {
+          // 竖屏
+          console.log('shu')
+        } else {
+          // 横屏
+          console.log('heng')
+        }
+      }
+      onMatchMeidaChange(mql);
+      mql.addListener(onMatchMeidaChange);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
